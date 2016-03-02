c02f2bc07d2f498a3d12365fd45a33315404ecf0
grimmdude <grimmdude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-02 02:58:40 +0000

    Version 1.6.5
    git-svn-id: https://plugins.svn.wordpress.org/recent-tweets-widget/trunk@1362265 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recent-tweets.php
+++ recent-tweets.php
@@ -4,7 +4,7 @@
 Plugin Name: Recent Tweets Widget
 Plugin URI: http://wordpress.org/extend/plugins/recent-tweets-widget/
 Description: Recent Tweets Widget plugin for Twitter API v1.1 with Cache. It uses the new Twitter API v1.1 and stores tweets in the cache. It means that it will read status messages from your database and it doesn't query Twitter.com for every page load so you won't be rate limited. You can set how often you want to update the cache.
-Version: 1.6.4
+Version: 1.6.5
 Author: Noah Kagan
 Author URI: http://sumome.com
 */
