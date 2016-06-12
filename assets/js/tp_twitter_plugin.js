02cdb1483266d5eeea80a9f00aa75191728fbe56
Sopan <khoslasopan@gmail.com>
2016-06-12 20:20:45 +0530

    Refers to DOM elements via classes

--- assets/js/tp_twitter_plugin.js
+++ assets/js/tp_twitter_plugin.js
@@ -1,17 +1,19 @@
 function update_twitter_auth(arg) {
-    jQuery("#widget-tp_widget_recent_tweets-2-consumerkey").prop('disabled', arg);
-	jQuery("#widget-tp_widget_recent_tweets-2-consumersecret").prop('disabled', arg);
-	jQuery("#widget-tp_widget_recent_tweets-2-accesstoken").prop('disabled', arg);
-	jQuery("#widget-tp_widget_recent_tweets-2-accesstokensecret").prop('disabled', arg);
+    jQuery(".rtw-consumerkey").prop('disabled', arg);
+	jQuery(".rtw-consumersecret").prop('disabled', arg);
+	jQuery(".rtw-accesstoken").prop('disabled', arg);
+	jQuery(".rtw-accesstokensecret").prop('disabled', arg);
 }
 
 
 jQuery(function() {
-	if(jQuery("#widget-tp_widget_recent_tweets-2-loklak_api").prop('checked')){
+	console.log("init");
+	if(jQuery(".rtw-loklak_api").prop('checked')){
+		console.log("init");
 		update_twitter_auth(true);
 	}
-	
-    jQuery("#widget-tp_widget_recent_tweets-2-loklak_api").live('change', function() {
+
+    jQuery(".rtw-loklak_api").live('change', function() {
     	if(jQuery(this).is(':checked')){
 	    	update_twitter_auth(true);
 	    }
@@ -19,4 +21,5 @@ jQuery(function() {
 	    	update_twitter_auth(false);
 	    }
 	});
+	
 });
