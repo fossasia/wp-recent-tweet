c3668663680282abde4a6598b86b36e5d0989393
Sopan <khoslasopan@gmail.com>
2016-06-17 02:30:54 +0530

    Adds widget initials to make function call unique

--- assets/js/tp_twitter_plugin.js
+++ assets/js/tp_twitter_plugin.js
@@ -1,4 +1,4 @@
-function update_twitter_auth(arg) {
+function rtw_update_twitter_auth(arg) {
     jQuery(".rtw-consumerkey").prop('disabled', arg);
 	jQuery(".rtw-consumersecret").prop('disabled', arg);
 	jQuery(".rtw-accesstoken").prop('disabled', arg);
@@ -10,15 +10,15 @@ jQuery(function() {
 	console.log("init");
 	if(jQuery(".rtw-loklak_api").prop('checked')){
 		console.log("init");
-		update_twitter_auth(true);
+		rtw_update_twitter_auth(true);
 	}
 
     jQuery(".rtw-loklak_api").live('change', function() {
     	if(jQuery(this).is(':checked')){
-	    	update_twitter_auth(true);
+	    	rtw_update_twitter_auth(true);
 	    }
 	    else {
-	    	update_twitter_auth(false);
+	    	rtw_update_twitter_auth(false);
 	    }
 	});
 	
